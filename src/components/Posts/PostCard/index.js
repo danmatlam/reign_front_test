d288c359582a97009f75fe26a09b474f0e2eecd9
danielarelam@gmail.com <danmat_gd@hotmail.com>
2022-05-23 00:01:11 -0500

    chore: pagination fix

--- src/components/Posts/PostCard/index.js
+++ src/components/Posts/PostCard/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import styled from 'styled-components'
 import { timeSince } from '../../../utils';
 import IconSwitcher from '../../IconSwitcher';
